b0140acbb90ac5fa0c81bc5aa1353b4422d3dd53
barcol <barcol1@interia.pl>
2019-05-14 15:46:44 +0200

    thickening the list works based on 3 lists, instead of creating an dict for every iter

--- FP/src/main.py
+++ FP/src/main.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -58,17 +58,22 @@ def interpolate(temp: pd.Series, data_to_interpolate: pd.Series, value: int):
 
 
 def thicken_list(thin_table: pd.DataFrame, start_point: int, end_point: int):
+    temps = []
+    cps = []
+    enthalpys = []
     for temp in range(int(start_point), int(end_point)):
         if temp not in thin_table["temp"]:
-            pass
-            # add new row that thickenes the empty space
-    return thin_table
+            temps.append(temp)
+            cps.append(interpolate(thin_table["temp"], thin_table["cp"], temp))
+            enthalpys.append(interpolate(thin_table["temp"], thin_table["enthalpy"], temp))
+    thin_table = thin_table.append(pd.DataFrame({"temp": temps, "cp": cps, "enthalpy": enthalpys}), ignore_index=True)
+    return thin_table.sort_values("temp")
 
 
 def add_phase_transition(data: pd.DataFrame, t_start: int, t_end: int, function: int):
-    value_list = thicken_list(data, t_start, t_end)
+    data = thicken_list(data, t_start, t_end)
     # b bla bla add function to list bla bla
-    return value_list
+    return data
 
 
 if __name__ == "__main__":
@@ -76,14 +81,11 @@ if __name__ == "__main__":
     enthalpy_data_frame = pd.DataFrame({"temp": temp_list, "cp": cp_list})
     repair_types(enthalpy_data_frame)
     enthalpy_data_frame = prepare_enthalpy(enthalpy_data_frame)
-    print(enthalpy_data_frame)
+    # print(enthalpy_data_frame)
+    # plt.plot(enthalpy_data_frame["enthalpy"], enthalpy_data_frame["temp"])
+    enthalpy_data_frame = add_phase_transition(enthalpy_data_frame, 100, 200, 1)
+    enthalpy_data_frame.reindex()
+    enthalpy_data_frame.to_csv(r"wyniki.txt")
     plt.plot(enthalpy_data_frame["enthalpy"], enthalpy_data_frame["temp"])
     plt.show()
-    thickened_list = add_phase_transition(enthalpy_data_frame, 100, 200, 1)
-    plt.show()
-    print(interpolate(enthalpy_data_frame["temp"], enthalpy_data_frame["cp"], 100))
-    print(interpolate(enthalpy_data_frame["temp"], enthalpy_data_frame["cp"], 200))
-    print(interpolate(enthalpy_data_frame["temp"], enthalpy_data_frame["cp"], 300))
-    print(interpolate(enthalpy_data_frame["temp"], enthalpy_data_frame["enthalpy"], 100))
-    print(interpolate(enthalpy_data_frame["temp"], enthalpy_data_frame["enthalpy"], 200))
-    print(interpolate(enthalpy_data_frame["temp"], enthalpy_data_frame["enthalpy"], 300))
+
